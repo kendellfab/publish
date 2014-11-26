69a349df679247cf404ee896b6adc9b9a722916b
Kendell Fabricius <me@kendellfabrici.us>
2014-11-25 21:46:03 -0700

    Starting with a setup route & template.

--- handlers/admin/admin_general.go
+++ handlers/admin/admin_general.go
@@ -18,8 +18,24 @@ func NewAdminGeneral(base *AdminBase, rm usecases.RepoManager) AdminGeneral {
 
 func (a AdminGeneral) RegisterRoutes(app *milo.Milo) {
 	app.Route("/admin", []string{"Get"}, a.authMid(a.handleAdmin))
+	app.Route("/setup", []string{"Get"}, a.handleSetup)
+	app.Route("/login", []string{"Get"}, a.handleLogin)
+	app.Route("/logout", []string{"Get"}, a.handleLogout)
 }
 
 func (a AdminGeneral) handleAdmin(w http.ResponseWriter, r *http.Request) {
 	a.RenderTemplates(w, r, nil, "base.tpl", "index.tpl")
+}
+
+func (a AdminGeneral) handleSetup(w http.ResponseWriter, r *http.Request) {
+	// a.RenderMessage(w, r, "Setup")
+	a.RenderTemplates(w, r, nil, "setup.tpl")
+}
+
+func (a AdminGeneral) handleLogin(w http.ResponseWriter, r *http.Request) {
+	a.RenderMessage(w, r, "Login")
+}
+
+func (a AdminGeneral) handleLogout(w http.ResponseWriter, r *http.Request) {
+	a.RenderMessage(w, r, "Logout")
 }
